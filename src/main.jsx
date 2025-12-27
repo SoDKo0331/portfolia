1590b753c0a4ac4d92135b4302bb79c3a4c7a5dc
SoDkO <154625410+SoDKo0331@users.noreply.github.com>
2025-12-27 11:53:00 +0800

    Replace ArtHistoryQuiz with KillTracker component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ArtHistoryQuiz from "./components/FrenchColonialRights";
+import KillTracker from "./components/FrenchColonialRights";
 import "./index.css"; // <- THIS IS CRUCIAL
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ArtHistoryQuiz />
+    <KillTracker />
   </React.StrictMode>
 );
