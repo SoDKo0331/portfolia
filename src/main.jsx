406109bd55d9f6f1903819af360d71d24d7c8697
SoDkO <154625410+SoDKo0331@users.noreply.github.com>
2026-01-02 14:20:41 +0800

    Replace KillTracker with HeyMongol component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import KillTracker from "./components/FrenchColonialRights";
+import HeyMongol from "./components/FrenchColonialRights";
 import "./index.css"; // <- THIS IS CRUCIAL
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <KillTracker />
+    <HeyMongol />
   </React.StrictMode>
 );
